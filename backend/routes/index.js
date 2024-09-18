ab28363cbff20a5556f6637245a74dc556a1845c
arseth229 <146048786+arseth229@users.noreply.github.com>
2024-09-18 16:14:48 -0500

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 //backend/routes/index.js
 const express = require ('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
 
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -9,5 +11,7 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-TOKEN': csrfToken
     });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
