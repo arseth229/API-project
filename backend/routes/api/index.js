a424d6c16c5488cd394e7a55b6bb9621e3f01122
arseth229 <146048786+arseth229@users.noreply.github.com>
2024-09-18 19:01:33 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require('../../utils/auth.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
-const { restoreUser } = require('../../utils/auth.js');
+
 
 router.get(
     '/restore-user',
@@ -33,6 +36,14 @@ router.get(
 );
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 
 module.exports = router;
